c80ec9237b3b781173be021dc98f0a984307e51e
Ahmad Ali <Ahmadsaligithub>
2024-02-26 17:16:52 +0500

    first commit

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -4,9 +4,10 @@ import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
   return (
-    <div className="errorMSG">
+    <div className="pageNotFound">
       <ContentWrapper>
-        <h1>404 ERROR</h1>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
       </ContentWrapper>
     </div>
   );
