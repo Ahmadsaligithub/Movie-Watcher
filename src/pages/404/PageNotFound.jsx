70ed47d802df1763d44e61ddc22b6f559678efb1
Ahmad Ali <Ahmadsaligithub>
2024-02-26 17:13:07 +0500

    first commit

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import "./PageNotFound.css"
+import React from "react";
+import "./PageNotFound.css";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div className="errorMSG">
+      <ContentWrapper>
+        <h1>404 ERROR</h1>
+      </ContentWrapper>
+    </div>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
